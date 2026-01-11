c2e41749cf47006e11073639405d9c207c58bdfe
AndriiShybaiev <60670387+AndriiShybaiev@users.noreply.github.com>
2026-01-11 12:33:56 +0100

    AC4.2 etapa 2: formulario con select de tipo de estudios

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-
 import './App.css'
 import EnrolmentForm from "./components/EnrolmentForm/EnrolmentForm.tsx";
+import React, { useState } from "react";
 
 function App() {
+    const [program, setProgram] = useState("UG");
+    const [enrolments, setEnrolments] = useState(0);
+
+    const handleChangeEnrolments = (updateEnrolments: number) => {
+        setEnrolments(updateEnrolments);
+    };
+
+    const handleChangeProgram = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setProgram(event.target.value);
+    };
+    //etapa 2 - formulario con select de tipo de estudios
     return (
-        <EnrolmentForm/>
-    )
+        <div className="App">
+            <div className="programs">
+                <label>Selecciona el tipo de estudio:</label>
+                <select
+                    className="appDropDowns"
+                    onChange={handleChangeProgram}
+                    value={program}
+                >
+                    <option value="UG">Grado</option>
+                    <option value="PG">Postgrado</option>
+                </select>
+            </div>
+
+            <div className="enrolments">
+                Matriculaciones actuales: {enrolments}
+            </div>
+
+            <EnrolmentForm
+                chosenProgram={program}
+                currentEnrolments={enrolments}
+                onChangeEnrolments={handleChangeEnrolments}
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/EnrolmentForm/EnrolmentForm.tsx
+++ src/components/EnrolmentForm/EnrolmentForm.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
-function EnrolmentForm() {
+import React, { type FormEvent, useState } from 'react';
+import './EnrolmentForm.css';
+
+interface EnrolmentFormProps {
+    chosenProgram: string;
+    currentEnrolments: number;
+    onChangeEnrolments: (updateEnrolments: number) => void;
+}
+
+function EnrolmentForm(props: EnrolmentFormProps) {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [welcomeMessage, setWelcomeMessage] = useState("");
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault(); //prevenimos recarga al submit
+        setWelcomeMessage(`Bienvenido/a ${firstName} ${lastName}`);
+        props.onChangeEnrolments(props.currentEnrolments + 1);
+    };
+    //etapa 2
     return (
         <div>
-            <form className="enrolForm">
-                <h2>Datos de estudiante</h2>
-                <label>Nombre</label>
-                <input type="text" name="fname"/>
-                <br/>
+            <form className="enrolForm" onSubmit={handleSubmit}>
+                <h1>Datos del estudiante - {props.chosenProgram}</h1>
+
+                <label>Nombre:</label>
+                <input
+                    type="text"
+                    name="fname"
+                    onBlur={(event) => setFirstName(event.target.value)}
+                />
+                <br />
+
                 <label>Apellidos:</label>
-                <input type="text" name="lname"/>
-                <br/>
-                <br/>
-                <input type="submit" value="Registrar"/>
+                <input
+                    type="text"
+                    name="lname"
+                    onBlur={(event) => setLastName(event.target.value)}
+                />
+                <br /><br />
+
+                <input type="submit" value="Registrar" />
+
+                <label className="message">{welcomeMessage}</label>
             </form>
         </div>
     );
 }
+
 export default EnrolmentForm;
